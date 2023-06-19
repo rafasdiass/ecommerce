332cf6331ac852fdb74a6aa791d7a8a904b9b7fb
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-06-19 00:19:08 -0300

    atualizacao

---
+++ src/app/dashboard/template/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+  constructor(private router: Router) {}
+
+  onNavigate(path: string): void {
+    this.router.navigateByUrl(path);
+  }
+}
